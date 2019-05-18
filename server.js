1dae45ba0d0a1e854d46e47b3565ae34b279ce77
laurence-liu <laurence.liuuu@gmail.com>
2019-05-18 11:17:37 -0700

    Add signin API
    http://domain.com/signin (POST)
    Input: email, password

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
 const knex = require('knex')
 
+const signin = require('./controllers/signin')
 const register = require('./controllers/register')
 
 const db = knex({
@@ -21,6 +22,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.listen(3000, () => {
