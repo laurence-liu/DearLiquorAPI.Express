86001fb70821a33e83a5f37f979e1aeb3cc67c54
laurence-liu <laurence.liuuu@gmail.com>
2019-05-31 16:37:01 -0700

    Add pagination for cocktails
    http://domain.com/cocktails?page=x (GET)
    Query: page

--- controllers/cocktails.js
+++ controllers/cocktails.js
@@ -1,7 +1,15 @@
 const handleCocktails = (req, res, db) => {
-    const { page } = req.body;
-    db.select('*').from('cocktails').limit(10).then(cocktail => {
-        res.json(cocktail)
+    const { page } = req.query;
+    
+    var from = page ? 25 * (page - 1) + 1 : 1
+    var end = page ? 25 * page : 25
+    var nextToken = page ? parseInt(page) + 1 : 2
+    var resultObj = {}
+
+    db.select('*').from('cocktails').whereBetween('id', [from, end]).then(cocktail => {
+        resultObj['cocktail'] = cocktail
+        resultObj['nextToken'] = nextToken
+        res.json(resultObj)
     }).
     catch(err => res.status(400).json('error getting cocktails'))
 }
